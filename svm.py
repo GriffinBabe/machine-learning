1112ee0265de4afe4f0279c6dd54ac0e36b71301
GriffinBabe <darius.couchard@hotmail.com>
2019-10-03 11:08:17 +0200

    Finished SVM

--- svm.py
+++ svm.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+from numpy.linalg import norm
 
 style.use('ggplot')
 
@@ -37,7 +38,93 @@ class Support_Vector_Machine:
 		step_sizes =	[self.max_feature_value * 0.1,
 						self.max_feature_value * 0.01,
 						self.max_feature_value * 0.001]
+		#optimisation step sizes above
+		b_range_multiple = 5
+		b_multiple = 5
+		latest_optimum = self.max_feature_value*10
+
+		for step in step_sizes:
+			w = np.array([latest_optimum, latest_optimum])
+			optimized = False
+			while not optimized:
+				# we iterate over all the b options
+				for b in np.arange(-1*(self.max_feature_value*b_range_multiple),
+										self.max_feature_value*b_range_multiple,
+										step*b_multiple):
+					for transformation in transforms:
+						w_t = w * transformation
+						found_option = True
+						for i in self.data:
+							for xi in self.data[i]:
+								yi = i
+								if not yi*(np.dot(w_t, xi)+b)>=1:
+									found_option = False
+						if found_option:
+							opt_dict[norm(w_t)] = [w_t, b]
+				if w[0] < 0: # if we found a negative w
+					optimized = True
+					print('optimized a step')
+				else:
+					w = w - step
+
+				norms = sorted([n for n in opt_dict]) # we sort of opt_dict by key
+				opt_choice = opt_dict[norms[0]] # we take the w_t and b values with the lowest norm
+				self.w = opt_choice[0]
+				self.b = opt_choice[1]
+				latest_optimum = opt_choice[0][0]+step*2
+
+
+
 	def predict(self, features):
 		# sign (w.x + b) = in which side of the plan are we
 		classification = np.sign(np.dot(np.array(features), self.w)+self.b)
+		if classification != 0 and self.visualization:
+			self.ax.scatter(features[0], features[1], s=200, marker='*', c=self.colors[classification])
+		else:
+			print('featureset', featureset, ' is on the decision boundary')
 		return classification
+
+	def visualize(self):
+		[[self.ax.scatter(x[0], x[1], s=100, color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+
+		def hyperplane(x,w,b,v):
+			return (-w[0]*x-b+v) / w[1]
+
+		datarange = (self.min_feature_value*0.9,self.max_feature_value*1.1)
+
+		hyp_x_min = datarange[0]
+		hyp_x_max = datarange[1]
+		psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+		psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+		self.ax.plot([hyp_x_min,hyp_x_max], [psv1,psv2], "k")
+		 # w.x + b = -1
+		# negative sv hyperplane
+		nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+		nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+		self.ax.plot([hyp_x_min,hyp_x_max], [nsv1,nsv2], "k")
+
+		# w.x + b = 0
+		# decision
+		db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+		db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+		self.ax.plot([hyp_x_min,hyp_x_max], [db1,db2], "g--")
+
+		plt.show()
+
+
+svm = Support_Vector_Machine()
+svm.train(data=data_dict)
+
+predict_us = [[0,10],
+              [1,3],
+              [3,4],
+              [3,5],
+              [5,5],
+              [5,6],
+              [6,-5],
+              [5,8]]
+
+for p in predict_us:
+    svm.predict(p)
+
+svm.visualize()
